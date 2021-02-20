20396d94edacccf5504cd42373a9ce1db8aeb1f7
suhachakka <suhasinichakka@gmail.com>
2021-02-20 16:30:44 -0600

    finished studio spaday user signup starter

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,13 +1,33 @@
 package org.launchcode.spaday.controllers;
 
+import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
-    public String displayUserForm(){
+
+    @GetMapping
+    public String displayUserForm(Model model){
+        model.addAttribute("title", "Add User");
         return "user/add";
     }
+   @PostMapping
+   public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        if(user.getPassword().equalsIgnoreCase(verify)){
+            model.addAttribute("message","Welcome " + user.getUsername());
+        }else{
+            model.addAttribute("errorMsg","Password didn\'t match");
+            model.addAttribute("username",user.getUsername());
+            model.addAttribute("email",user.getEmail());
+            return  "user/add";
+        }
+    return "user/index";
+   }
 
 }
